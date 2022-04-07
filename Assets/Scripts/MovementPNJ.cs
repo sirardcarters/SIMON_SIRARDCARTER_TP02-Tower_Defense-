c2a22d01602c277b67d5a230e86fbcf720b31287
sirardcarters <1955437@cegepat.qc.ca>
2022-04-07 08:50:35 -0400

    modif hierarchy

--- Assets/Scripts/MovementPNJ.cs
+++ Assets/Scripts/MovementPNJ.cs
@@ -3,26 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class testforpnj : MonoBehaviour
+public class MovementPNJ : MonoBehaviour
 {
     private NavMeshAgent agent;
-    private bool isAgentBusy = false;
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+
+        Vector3 newDestination = new Vector3(16.4f, 0f, -52.8f);
+
+        agent.SetDestination(newDestination);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!isAgentBusy)
-        {
-            Vector3 newDestination = new Vector3(25f, 1f, 35f);
 
-            agent.SetDestination(newDestination);
-
-            isAgentBusy = true;
-        }
+        
     }
 }
