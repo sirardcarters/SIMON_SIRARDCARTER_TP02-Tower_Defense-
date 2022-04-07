0ca8a9660421d9527d76872178df4561118b9145
sirardcarters <1955437@cegepat.qc.ca>
2022-04-07 09:18:25 -0400

    Ajout d'un script pour le UI
    Seulement Pause à été fait

--- Assets/Scripts/UiManager.cs
+++ Assets/Scripts/UiManager.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class UiManager : MonoBehaviour
 {
+    public static bool GameIsPaused = false;
+    public Button pause;
+    public Button play;
+    //public GameObject PauseMenuUi;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        pause.onClick.AddListener(PauseGame);
+        play.onClick.AddListener(ResumeGame);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
     void PauseGame()
     {
-        Time.timeScale = 0;
+        //PauseMenuUi.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
     }
     void ResumeGame()
     {
-        Time.timeScale = 1;
+        //PauseMenuUi.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
     }
 }
